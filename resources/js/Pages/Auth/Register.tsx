f20f432c33f3358f4d82b62d59e7a9215eb39db0
Gabriel <gabrieldebem72@gmail.com>
2022-10-09 21:55:01 -0300

    Revert "add initial frontend with react"
    This reverts commit b325bf06e5b126d4903b8b0d05aa198a3fd7d6fa.

--- resources/js/Pages/Auth/Register.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Register() {
-    return (
-        <div>
-            Tela de cadastro
-        </div>
-    );
-}
--- resources/js/app.tsx
+++
@@ -1,9 +0,0 @@
-import './bootstrap';
-import '../css/app.css';
-
-import React from 'react';
-import App from "./ReactApp";
-import {createRoot} from "react-dom/client";
-
-
-createRoot(document.getElementById('app')).render(<App/>);
